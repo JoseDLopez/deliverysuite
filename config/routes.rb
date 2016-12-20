1e54daf096b7f2edc62607a8a8760a9897a9f83d
JoseDLopez <josed-lopez@hotmail.com>
2016-12-20 20:35:40 -0300

    Controller pages creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/status'
+
+  get 'pages/livemap'
+
+  root 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
