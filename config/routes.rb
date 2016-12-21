a1e8f2199f275a40209cc6b3826dfd52e750839d
JoseDLopez <josed-lopez@hotmail.com>
2016-12-21 02:05:19 -0300

    routes creation for product, order, client

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :employees
+
   get 'pages/home'
 
   get 'pages/status'
@@ -9,6 +10,12 @@ Rails.application.routes.draw do
   get 'pages/livemap'
 
   root 'pages#home'
+
+  resources :products, only: [:index, :show]
+
+  resources :clients, only: [:index, :show] do
+  	resources :order, only: [:index, :show, :create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
