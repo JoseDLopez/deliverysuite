512741e49163febfa4fa8ee8784af3187178d970
JoseDLopez <josed-lopez@hotmail.com>
2016-12-21 00:17:57 -0300

    Relationship between Order model and Employee model ok

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,10 @@
 class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	devise :database_authenticatable, :registerable,
+	:recoverable, :rememberable, :trackable, :validatable
+
+	has_many :order_employees
+	has_many :orders, through: :order_employees
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,7 @@ class Order < ApplicationRecord
   belongs_to :zone
   has_many :order_products
   has_many :products, through: :order_products
+  has_many :order_employees
+  has_many :employees, through: :order_employees
   has_one :payment
 end
