2ddf432959e4bbc042d7f3c0861652a6422c9248
JoseDLopez <josed-lopez@hotmail.com>
2016-12-23 20:24:19 -0300

    Product model in admin totally ok, only missing uploading, and showing photo

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Product do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
-	permit_params :name, :description, category_ids:[]
+	permit_params :name, :description, :time, :price, :picture, category_ids:[]
 #
 # or
 #
@@ -14,14 +14,59 @@ ActiveAdmin.register Product do
 #   permitted
 # end
 
+
 	form do |f|
-		inputs "Add a new category" do
+		inputs "Agrega un nuevo producto" do
 			input :name
 			input :description
-			input :category_ids, as: :select, collection: Category.all, multiple: true
+			input :price
+			input :time
+			input :picture
+			# input :category_ids, as: :select, collection: Category.all, multiple: true
+			input :categories, as: :check_boxes, collection: Category.all
 		end
 		actions
 	end
 
+	show do
+		panel "Tabla del Producto" do
+			table_for product do
+				column :name
+				column :description
+				column :picture
+			end
+		end
+
+		panel "Categorías del Producto" do
+			table_for product.categories do
+				column "name" do |category|
+					link_to category.name, admin_category_path(category)
+				end
+				column :description
+			end
+		end
+		active_admin_comments
+	end
+
+	sidebar "Details", only: :show do
+		attributes_table_for product do
+			row :name
+			row :price
+			row :time
+			row :description
+			row :picture
+		end
+	end
+
+	filter :name
+	filter :categories
+	filter :time
+	filter :price
+	filter :created_at
 
 end
+
+
+
+
+
