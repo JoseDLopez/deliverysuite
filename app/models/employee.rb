9148c26c44c1a5dcaf23305fe649e9a1aa31b8ae
JoseDLopez <josed-lopez@hotmail.com>
2016-12-22 21:31:36 -0300

    Rut validator working ok

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,14 +7,7 @@ class Employee < ApplicationRecord
 	has_many :order_employees
 	has_many :orders, through: :order_employees
 
-	validate :rut_validation
+	validates :rut, rut: true
 	validates_uniqueness_of :rut
-
-	def rut_validation
-		if rut == "hola"
-		errors.add(:rut, "can't be destroyed cause x,y or z")
-		throw(:abort)
-		end
-	end
 
 end
