7e472b7548d355610ef67e85696da678101c59fa
JoseDLopez <josed-lopez@hotmail.com>
2017-01-05 22:46:05 -0300

    Pending to modify dots entry

--- app/admin/zone.rb
+++ app/admin/zone.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Zone do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
-#
+	permit_params :name, :description, :price
 # or
 #
 # permit_params do
@@ -25,5 +25,41 @@ ActiveAdmin.register Zone do
 	filter :name
 	filter :price
 	filter :created_at
+
+  show do
+  	panel "Ubicación en el Mapa:" do
+	  	# renders app/views/admin/posts/_some_partial.html.erb
+	    render 'show_zone_map'
+  	end
+
+  end
+
+  sidebar "Details", only: :show do
+    attributes_table_for zone do
+      row :name
+      row :price
+      row :description
+      row :created_at
+    end
+  end
+
+  #Editing form for new zone:
+
+	# form do |f|
+	# 	inputs 'Details' do
+	# 		input :name
+	# 		input :price
+	# 	end
+	# 	panel 'Area de Cobertura' do
+			
+	# 	end
+	# 	inputs 'Content', :description
+	# 	para "Press cancel to return to the list without saving."
+	# 	actions
+	# end
+
+	form partial: 'new_zone_form'
+
+
 
 end
