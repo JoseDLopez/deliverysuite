c866c14462b7e38d33b8403a3b612157b4b320d9
JoseDLopez <josed-lopez@hotmail.com>
2017-02-07 01:49:24 -0300

    Se ven los pedidos que estan pendiente de pago en el mapa

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,10 +10,22 @@ class PagesController < ApplicationController
 	@customers = Client.all
 	@zones = Zone.all
 	@orders = Order.all
-	@hash = Gmaps4rails.build_markers(@customers) do |customer, marker|
-		marker.lat customer.latitude
-		marker.lng customer.longitude
-		marker.title customer.name
+	@clientes = []
+
+	@orders.each do |o|
+		if o.payment == nil
+			@clientes.push({"name" => o.client.name, "latitude" => o.client.latitude, "longitude" => o.client.longitude, "order" => o.id, "client" => o.client.id})
+		end
+
 	end
+
+
+	# @hash = clientes.each do |customer, marker|
+	# 	marker.lat customer["latitude"]
+	# 	marker.lng customer["longitude"]
+	# 	marker.title customer["name"]
+	# 	marker.order customer["order"]
+	# 	marker.client customer["client"]
+	# end
   end
 end
